691136a388274e00bdf18a736b5e7056fe1716bf
Ash Mackay <ash.mackay85@gmail.com>
2022-04-12 12:32:04 +0100

    Strict Compilation

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 const button = document.querySelector("button")!;
 
+function clickHandler(message: string) {
+  console.log("Clicked! " + message);
+}
+
 // a comment
-button.addEventListener("click", () => {
-  console.log("Clicked!");
-});
+// only require if check or !, both are used to show that either option works
+if (button) {
+  button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
+}
